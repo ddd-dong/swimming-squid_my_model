5f9239bef0190663fa894e387ac85b9763c40d9b
chenfu <kye1234321@gmail.com>
2024-01-10 11:26:30 +0800

    add make dir in train_PPO.py and train_DQN.py

--- ml/train_DPN.py
+++ ml/train_DPN.py
@@ -1,7 +1,12 @@
+import os
+
 from stable_baselines3 import DQN
 from gym_Environment import Environment as env
 
 if __name__ == '__main__':
+    folder_path = './save'
+    os.makedirs(folder_path, exist_ok=True)
+    
     env = env()
     model = DQN("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=100_000, log_interval=1)
